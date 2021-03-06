5be0925bc357e16110a303221d54af252ddd169c
thanhlong98 <ntl2898js@gmail.com>
2021-03-06 17:33:21 +0700

    remove tailwind

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -5,11 +5,7 @@ type Props = {
 }
 
 const Container: React.FC<Props> = ({ children, className }) => {
-  return (
-    <div className={`md:w-4/5 sm:w-full mx-auto px-2 ${className}`}>
-      {children}
-    </div>
-  )
+  return <div>{children}</div>
 }
 
 export default Container
---
+++ src/pages/todos/index.tsx
@@ -0,0 +1,8 @@
+import { MainLayout } from '@layouts'
+import React from 'react'
+
+const TodosPage = () => {
+  return <MainLayout hasFooter={false}>Hello World</MainLayout>
+}
+
+export default TodosPage
